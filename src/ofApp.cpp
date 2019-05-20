fca248414c6c8466901e702f2ff80c084f636652
RieFujimoto <50042016+RieFujimoto@users.noreply.github.com>
2019-05-20 17:53:08 +0900

    first

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,130 +3,77 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetBackgroundColor(0);
-    //    ofSetFrameRate();
-    move = ofVec3f(0, 0, 0);
+    move = ofVec2f(0, 0);
     //    cout << ofGetHeight() << ", " << ofGetWidth() << endl;
 }
 
 //--------------------------------------------------------------
-void ofApp::seek(ofVec3f target){
-    ofVec3f desired = target - location;
-    desired.normalize();
-    desired *= maxspeed;
-    
-    ofVec3f steer = desired - velocity;
-    steer.limit(maxforce);
-    
-    applyForce(steer);
-}
-
-//--------------------------------------------------------------
-void ofApp::applyForce(ofVec3f force){
-    acceleration += force;
-}
-
-//--------------------------------------------------------------
 void ofApp::update(){
-    
-    velocity += acceleration;
-    velocity.limit(maxspeed);
-    location += velocity;
-    acceleration *= 0;
-    
-    if (wingTheta < -60) {
-        wingUp = true;
-    }
-    if (wingTheta > 60) {
-        wingUp = false;
-    }
-    if (wingUp) {
-        wingTheta += wingSpeed;
-    } else {
-        wingTheta -= wingSpeed;
-    }
-    
-    move.x  = -ofGetWidth()/2 +ofGetFrameNum()*0.1;
-    move.y  = ofGetHeight()/2 - ofGetFrameNum()*0.1;
+    move.x = ofGetFrameNum()*0.1;
+    move.y = ofGetFrameNum()*0.1;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    location = ofVec3f(move.x, move.y, move.z);
+//    ofPushStyle();
+//    ofRotateDeg(ofGetFrameNum()*0.1, ofGetFrameNum()*0.1, ofGetFrameNum()*0.1, ofGetFrameNum()*0.1);
+//    draw_sakura(50, 50, 100);
+//    ofPopStyle();
+//
+//    ofPushStyle();
+//    ofRotateDeg(-ofGetFrameNum()*0.2, ofGetFrameNum()*0.3, ofGetFrameNum()*0.1, ofGetFrameNum()*0.2);
+//    draw_sakura(100, 100, 200);
+//    ofPopStyle();
     
-    //    maxspeed = ofRandom(10) + 2;
-    //    maxforce = ofRandom(0.002) + 0.001;
-    //    wingSpeed = ofRandom(1) + 1;
-    //    wingSpeed = 1;
-    wingTheta = 30;
-    wingUp = true;
+    draw_sakura(50, 50, 100);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_sakura(int x, int y, int big){
+    location = ofVec2f(x + move.x, y + move.y);
+//    location = ofVec2f(x , y);
     
-    size = ofRandom(0) + 50.0;
-    
-    cam.begin();
+    size = ofRandom(0) + big;
     
     ofPushMatrix();
     ofTranslate(location);
     
+    ofSetColor(250, 190, 230, 200);
+
     ofRotateDeg(ofGetFrameNum()*0.1, ofGetFrameNum()*0.1, ofGetFrameNum()*0.1, ofGetFrameNum()*0.1);
-    ofSetColor(200, 160, 200, 200);
     
-    // left wing
     ofPushMatrix();
     
     ofBeginShape();
-    ofVertex(0, 0, 0);
     ofVertex(size*0.4, 0, 0);
     ofVertex(size*0.37, size*0.25, 0);
+    ofVertex(size*0.3, size*0.38, 0);
     ofVertex(size*0.2, size*0.5, 0);
     ofVertex(0, size*0.3, 0);
-    ofEndShape();
-    
-    ofBeginShape();
-    ofVertex(0, 0, 0);
-    ofVertex(size*0.4, 0, 0);
-    ofVertex(size*0.37, -size*0.2, 0);
-    ofVertex(size*0.2, -size*0.4, 0);
-    ofVertex(0, -size*0.6, 0);
-    ofEndShape();
-    
-    ofPopMatrix();
-    
-    // rigtht wing
-    ofPushMatrix();
-    ofRotateYDeg(-wingTheta);
-    
-    ofBeginShape();
-    ofVertex(0, 0, 0);
-    ofVertex(-size*0.4, 0, 0);
-    ofVertex(-size*0.37, size*0.25, 0);
     ofVertex(-size*0.2, size*0.5, 0);
-    ofVertex(0, size*0.3, 0);
-    ofEndShape();
-    
-    ofBeginShape();
-    ofVertex(0, 0, 0);
+    ofVertex(-size*0.3, size*0.38, 0);
+    ofVertex(-size*0.37, size*0.25, 0);
     ofVertex(-size*0.4, 0, 0);
     ofVertex(-size*0.37, -size*0.2, 0);
-    ofVertex(-size*0.2, -size*0.4, 0);
+    ofVertex(-size*0.23, -size*0.4, 0);
     ofVertex(0, -size*0.6, 0);
+    ofVertex(size*0.23, -size*0.4, 0);
+    ofVertex(size*0.37, -size*0.2, 0);
     ofEndShape();
     
     ofPopMatrix();
-    
     ofPopMatrix();
-    
-    cam.end();
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    
+    mouse.x = x;
+    mouse.y = y;
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
-//    move.x = x;
-//    move.y = y;
+//    mouseX = x;
+//    mouseY = y;
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,27 +7,18 @@
 class ofApp : public ofBaseApp{
     
 public:
-    ofVec3f location;
-    ofVec3f velocity;
-    ofVec3f acceleration;
-    ofVec3f move;
-    
-    float maxspeed;
-    float maxforce;
-    
-    float wingTheta;
-    bool wingUp;
-    float wingSpeed;
+    ofVec2f location;
+    ofVec2f move;
+    ofVec2f mouse;
     float size;
-    
+
     void setup();
-    void seek(ofVec3f target);
-    void applyForce(ofVec3f force);
     void update();
     void draw();
     void mousePressed(int x, int y, int button);
     void mouseMoved(int x, int y);
+    void draw_sakura(int x, int y, int big);
     
-    ofEasyCam cam;
+//    ofEasyCam cam;
 };
 
